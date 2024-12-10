c49fccadabc4f7b412b5c78790707f6d1f7977f9
Bart Thate <rssbotd@gmail.com>
2024-12-10 16:22:21 +0100

    add face

---
+++ nixt/face.py
@@ -0,0 +1,16 @@
+# This file is placed in the Public Domain.
+# pylint: disable=W0611,W0401,W0614,W0622
+
+
+"interface"
+
+
+from . import console, control, daemon, object, persist, runtime
+
+
+from .console import *
+from .control import *
+from .daemon  import *
+from .object  import *
+from .persist import *
+from .runtime import *
