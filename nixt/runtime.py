d8805c116c2b8066f0487da75430b3828530ce9f
Bart Thate <rssbotd@gmail.com>
2025-01-06 11:35:34 +0100

    update tinder

--- nixt/runtime.py
+++ nixt/runtime.py
@@ -71,9 +71,9 @@ def command(bot, evt):
     if func:
         try:
             func(evt)
+            bot.display(evt)
         except Exception as ex:
             later(ex)
-    bot.display(evt)
     evt.ready()
 
 
@@ -240,7 +240,6 @@ class Output:
 
     def wait(self):
         self.dostop.wait()
-        print(self.oqueue.qsize())
 
 
 "reactor"
@@ -264,10 +263,6 @@ class Reactor:
             evt._ex = ex
             later(ex)
             evt.ready()
-
-    def display(self, evt):
-        for txt in evt.result:
-            self.raw(txt)
 
     def loop(self):
         while not self.stopped.is_set():
