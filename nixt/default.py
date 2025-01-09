8f75536994f8a7ca53acf46311e304b24da0996b
Bart Thate <rssbotd@gmail.com>
2025-01-09 06:04:24 +0100

    add default

---
+++ nixt/default.py
@@ -0,0 +1,35 @@
+# This file is placed in the Public Domain.
+
+
+"default values"
+
+
+class Default:
+
+    def __contains__(self, key):
+        return key in dir(self)
+
+    def __getattr__(self, key):
+        return self.__dict__.get(key, "")
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+
+class Config(Default):
+
+    pass
+
+
+def __dir__():
+    return (
+        'Config',
+        'Default'
+    )
+        
