15b05088d4ce5a3cfeb1b376bdc61cd2c26b02a4
Bart Thate <bthate@dds.nl>
2025-01-20 03:07:47 +0100

    update md5

--- nixt/command.py
+++ nixt/command.py
@@ -197,17 +197,16 @@ def spl(txt):
 
 
 MD5 = {
-    "cmd": "e78043b056cf96aaf89f1c7120c1cd2d",
-    "err": "5be6a5e9979ce54ee6732042e2f94ca0",
-    "fnd": "01bf50246b2359fb50b2525be1534224",
-    "irc": "cad25097e75b162e1fac703d48d3b97f",
-    "log": "8eb40f6b95daf57780d28d7f1b7a80c5",
-    "mod": "abb31624685eaa65b5d9d2aa93024347",
-    "opm": "cc5f3d72512e0cc07c0211ad4db173b8",
-    "req": "faddd66da68fbcb979b25297113916c6",
-    "rss": "abe8a2dae8de3827ba67ecb60f730676",
-    "thr": "e7c0a98c0eec0d2c8186ea23651ae7e2",
-    "upt": "22016f78b86dd0a4f4fa25b2de2ff76b"
+    "cmd": "ce5f48c564d09e79df52ee1130e6c3a2",
+    "err": "e66295ed4a34bbcca7547be6cbfe4c21",
+    "fnd": "ae1697c84c733c6190f69ebf7c1448d0",
+    "irc": "bc6e38c421c7f0362acf1706f5400937",
+    "log": "3e798f036d3fcaa19329486fca33fc20",
+    "mod": "905102ea6cd9a3a6e2a0f0bd3d73ebe5",
+    "req": "464fb82d2ec152bfcffbab99fb6de3af",
+    "rss": "c9e2aa7c72ee1177e4f24907beffc93d",
+    "thr": "deb19e3c6238e80844e29bf9c0cc7d74",
+    "upt": "3d1605d335814d738393d29acc073ec1"
 }
 
 
