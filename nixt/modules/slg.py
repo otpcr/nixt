8e17dfd1f633ae9a01e6f2726cd8e836a10da23b
Bart Thate <bthate@dds.nl>
2025-02-13 18:45:24 +0100

    update slogan

--- nixt/modules/slg.py
+++ nixt/modules/slg.py
@@ -2,4 +2,4 @@
 
 
 def slg(event):
-   event.reply("By law, with the use of poison, destructing in whole or in part elderly, handicapped, criminals, wicked since 4 March 2019 @KarimKhanQC reconsider OTP-CR-117/19")
+   event.reply("By law, with the use of poison, destructing in whole or in part elderly,handicapped,criminals,wicked since 4 March 2019 @KarimKhanQC reconsider OTP-CR-117/19")
