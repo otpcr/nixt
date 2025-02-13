8f6c7415fff75428dbc3844cefa48d5a39af22ce
Bart Thate <bthate@dds.nl>
2025-02-13 16:21:34 +0100

    add slogan

---
+++ nixt/modules/slg.py
@@ -0,0 +1,5 @@
+# This file is placed in the Public Domain.
+
+
+def slg(event):
+   event.reply("By law, with the use of poison, destructing in whole or in part elderly, handicapped, criminals, wicked since 4 March 2019 @KarimKhanQC reconsider OTP-CR-117/19")
