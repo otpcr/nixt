ffc271362f97c891b8509837f39cbb0fb99035db
bart <bart@localhost.localdomain>
2025-02-19 09:28:31 +0100

    update slogan

--- nixt/modules/slg.py
+++ nixt/modules/slg.py
@@ -4,11 +4,8 @@
 "slogan"
 
 
+TXT = "By law using poison destructing in whole or in part elderly handicapped criminals wicked, @KarimKhanQC reconsider OTP-CR-117/19 4 March 2019 http://bit.ly/gcid3"
+
+
 def slg(event):
-   event.reply("By law, with the use of poison, destructing in whole or in part elderly,handicapped,criminals,wicked since 4 March 2019 @KarimKhanQC reconsider OTP-CR-117/19")
-
-
-def __dir__():
-    return (
-        'slg',
-   )
+    event.reply(TXT)
